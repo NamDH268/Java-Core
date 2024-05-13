3c461fa113b8c31d4f6a6bf20b7f9774aabe78d7
NamDH268 <namdao2608@gmail.com>
2024-05-13 19:43:08 +0700

    add

---
+++ src/main/java/Exercise01.java
@@ -0,0 +1,15 @@
+public class Exercise01 {
+    void Question01(){
+        float f1 = 5240.5f;
+        double f2 = 10970.055;
+
+        int i1 = (int) f1;
+        System.out.println(i1);
+
+        int i2 = (int) f2;
+        System.out.println(i2);
+    }
+    float Question04(int a, int b){
+        return (float) a/b;
+    }
+}
---
+++ src/main/java/Exercise04.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Exercise04 {
+    void Question01(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào 1 chuỗi kí tự: ");
+        String s = scanner.nextLine();
+        String[] s1 = s.split(" ");
+        int count = 0;
+        for (String s2 : s1){
+            if (!s2.isEmpty()){
+                count++;
+            }
+        }
+        System.out.println("Số từ: " + count);
+
+       /* int s2 = s.trim().split("\\s+").length;
+        System.out.println("s2 = " + s2);*/
+    }
+    void Question03(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tên: ");
+        String ten = scanner.nextLine();
+        String t1 = ten.substring(0,1).toUpperCase() + ten.substring(1).toLowerCase();
+        System.out.println("t1 = " + t1);
+    }
+    void Question04(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tên: ");
+        String ten = scanner.nextLine();
+        int length = ten.length();
+        for (int i = 0; i < length; i++) {
+            int n = i +1;
+            int c = ten.charAt(i);
+            System.out.printf("ký tự thứ %d la: %C%n", n,c);
+        }
+       /* String[] words = ten.split("");
+        for (String word : words){
+            System.out.println("word = " + word);
+        }*/
+    }
+    void Question05(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào họ: ");
+        String ho = scanner.nextLine();
+        System.out.println("Nhập vào tên: ");
+        String ten = scanner.nextLine();
+        System.out.println("họ và tên là: " + ho +" "+ ten);
+    }
+    void Question06(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào họ và tên đầy đủ: ");
+        String s = scanner.nextLine();
+        int indexOfFirstWhiteSpace = s.indexOf(' ');
+        int indexOfLastWhiteSpace = s.lastIndexOf(' ');
+        String firstName = s.substring(0, indexOfFirstWhiteSpace);
+        String lastName = s.substring(indexOfLastWhiteSpace +1 );
+        String middleName = s.substring(indexOfFirstWhiteSpace +1,indexOfLastWhiteSpace);
+        System.out.println("Họ là: " + firstName);
+        System.out.println("Tên đệm là: " + middleName);
+        System.out.println("Tên là: " + lastName);
+    }
+    void Question07(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào họ và tên đầy đủ: ");
+        String name = scanner.nextLine();
+        System.out.println("họ và tên = " + name.trim());
+
+
+        System.out.println("Nhập vào tên: ");
+        String ten = scanner.nextLine();
+        String t1 = ten.substring(0,1).toUpperCase() + ten.substring(1).toLowerCase();
+        System.out.println("t1 = " + t1);
+    }
+    void Question08(){
+
+    }
+}

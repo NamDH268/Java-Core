68eff8fbccb1291a75a8dba5cf93b3a1b802d1cf
NamDH268 <namdao2608@gmail.com>
2024-05-15 19:35:38 +0700

    add assginment-04

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -4,14 +4,28 @@ public class Group {
     String name;
     Account creator;
     LocalDate createdDate;
+    Account[] accounts;
 
-    Group(){
+    Group() {
     }
 
-    public Group(int id, LocalDate createdDate, String name, Account creator) {
-        this.id = id;
-        this.createdDate = createdDate;
+    Group(String name, Account creator, Account[] accounts, LocalDate createdDate) {
         this.name = name;
         this.creator = creator;
+        this.accounts = accounts;
+        this.createdDate = createdDate;
+    }
+
+    Group(String name, Account creator, String[] usernames, LocalDate createdDate) {
+        this.name = name;
+        this.creator = creator;
+        Account[] acc = new Account[usernames.length];
+        for (int i = 0; i < usernames.length; i++) {
+            Account a = new Account();
+            a.username = usernames[i];
+            acc[i] = a;
+        }
+        this.accounts = acc;
+        this.createdDate = createdDate;
     }
 }

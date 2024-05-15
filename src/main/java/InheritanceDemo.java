ea8483d84bd93e11edcf6c4f4c079f1afec19d90
NamDH268 <namdao2608@gmail.com>
2024-05-15 20:07:57 +0700

    create lesson-06

---
+++ src/main/java/InheritanceDemo.java
@@ -0,0 +1,13 @@
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        // Tính kế thừa
+        // Mục đích: Tái sử dụng code
+        // Từ khóa: extends
+        // Đơn kế thừa
+        Dog dog = new Dog("chihuahua", 3);
+        dog.run();
+
+        // this: tham chiếu đến class đó
+        // super: tham chiêu đến class cha
+    }
+}
